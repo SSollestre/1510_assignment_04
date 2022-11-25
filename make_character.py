e576f5f4ab98b3aaedf32f92f8ef77adc6064137
SSollestre <sean.sollestre@gmail.com>
2022-11-24 16:49:06 -0800

    Implement character creation

---
+++ make_character.py
@@ -0,0 +1,29 @@
+"""
+Emily Tran        A00990221
+Sean Sollestre    A01333807
+"""
+
+
+def make_character():
+    name = input("What is your name?\n")
+    character_board ={
+        "name": name,
+        "Strength": 10,
+        "Vitality": 10,
+        "Dexterity": 10,
+        "EXP": 0
+    }
+    return character_board
+
+
+# def display_character_info(char):
+#     name = character["name"]
+#     strength = character["Strength"]
+#     vitality = character["Vitality"]
+#     dexterity = character["Dexterity"]
+#     exp = character["EXP"]
+#     print("Name:%s\nStrength:%s\nVitality:%x\nDexterity:%s\nEXP:%s" % (name, strength, vitality, dexterity, exp))
+#
+#
+# character = make_character()
+# display_character_info(character)
