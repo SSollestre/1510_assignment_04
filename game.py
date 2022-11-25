d5eeb6b233616d5b02376d6ba8f72cd0d1c3fef0
SSollestre <sean.sollestre@gmail.com>
2022-11-24 16:14:21 -0800

    Add framework to game

--- game.py
+++ game.py
@@ -4,6 +4,30 @@ Sean Sollestre    A01333807
 """
 
 
+def game(): # called from main
+    rows = 5
+    columns = 5
+    board = make_board(rows, columns)
+    character = make_character("Player name")
+    achieved_goal = False
+    while not achieved_goal:
+        describe_current_location(board, character)
+        direction = get_user_choice( )
+        valid_move = validate_move(board, character, direction)
+        if valid_move:
+            move_character(character)
+            describe_current_location(board, character)
+            there_is_a_challenge = check_for_challenges()
+        if there_is_a_challenge:
+            execute_challenge_protocol(character)
+        if character_has_leveled():
+            execute_glow_up_protocol()
+            achieved_goal = check_if_goal_attained(board, character)
+        else:
+            print("A wall blocks your path.")
+    print("you have successfully escaped")
+
+
 def main():
     pass
 
