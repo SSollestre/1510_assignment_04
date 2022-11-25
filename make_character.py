a46b17a3cea3051758b4ffa972f21bd573dded21
SSollestre <sean.sollestre@gmail.com>
2022-11-24 17:11:04 -0800

    Implement basic encounter

--- make_character.py
+++ make_character.py
@@ -8,6 +8,7 @@ def make_character():
     name = input("What is your name?\n")
     character_board = {
         "name": name,
+        "Health": 50,
         "Strength": 10,
         "Vitality": 10,
         "Dexterity": 10,
@@ -41,9 +42,21 @@ def execute_glowup_protocol(char, check):
         char["Experience"] = 0
 
 
+def execute_challenge_protocol(char):
+    bandit = {
+        "Health": 50,
+        "Experience": 25
+    }
+    while bandit["Health"] > 0 and char["Health"] > 0:
+        action = input("What do you want to do?")
+        print("You %s" % action)
+        bandit["Health"] = 0
+
+
 character = make_character()
 display_character_info(character)
 character["Experience"] = 51
 status = character_has_leveled(character)
 execute_glowup_protocol(character, status)
 display_character_info(character)
+execute_challenge_protocol(character)
