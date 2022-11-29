87474525f799ef436835d8e8818add44e2e46ee0
SSollestre <sean.sollestre@gmail.com>
2022-11-29 08:15:28 -0800

    Added skills to character dictionary

--- make_character.py
+++ make_character.py
@@ -4,7 +4,7 @@ Sean Sollestre    A01333807
 """
 
 
-def make_character():
+def make_character():  #
     name = input("What is your name?\n")
     character_board = {
         "name": name,
@@ -15,7 +15,8 @@ def make_character():
         "dexterity": 10,
         "level": 1,
         "exp": 0,
-        "goal": False
+        "goal": False,
+        "skills": []
     }
     return character_board
 
@@ -23,6 +24,14 @@ def make_character():
 def attack(char, enemy):  #
     damage = char["strength"] - (0.25 * enemy["defense"])
     enemy["health"] -= damage
+    print(char["name"], "strikes", enemy["name"], "for", damage, "damage!")
+    return enemy
+
+
+def attack_skill(char, enemy):  #
+    damage = char["strength"] - (0.25 * enemy["defense"])
+    enemy["health"] -= damage * 2
+    print(char["name"], "strikes", enemy["name"], "for", damage, "damage!")
     print(char["name"], "strikes", enemy["name"], "for", damage, "damage!")
     return enemy
 
@@ -41,7 +50,7 @@ def return_entity(level):
     bandit_lieutenant = {
         "name": "Bandit Lieutenant",
         "health": 50,
-        "strength": 10,
+        "strength": 30,
         "defense": 10,
         "dexterity": 10,
         "level": 2,
@@ -50,8 +59,8 @@ def return_entity(level):
 
     bandit_chief = {
         "name": "Bandit Chief",
-        "health": 50,
-        "strength": 10,
+        "health": 150,
+        "strength": 20,
         "defense": 10,
         "dexterity": 10,
         "level": 3,
@@ -68,15 +77,16 @@ def return_entity(level):
         return bandit_chief
 
 
-def display_character_info(char):
+def display_character_info(char):  #
     name = char["name"]
     strength = char["strength"]
     defense = char["defense"]
     dexterity = char["dexterity"]
     exp = character["exp"]
     lvl = character["level"]
-    print("Name:%s\nStrength:%s\ndefense:%s\nDexterity:%s\nLevel:%s\nEXP:%s"
-          % (name, strength, defense, dexterity, lvl, exp) + "\n")
+    skills = character["skills"]
+    print("Name:%s\nStrength:%s\ndefense:%s\nDexterity:%s\nLevel:%s\nSkills:%s\nEXP:%s"
+          % (name, strength, defense, dexterity, lvl, skills, exp) + "\n")
 
 
 def character_has_leveled(char):
@@ -86,21 +96,26 @@ def character_has_leveled(char):
     return False
 
 
-def execute_glowup_protocol(char, check):
+def execute_glowup_protocol(char, check):  #
+    double_strike = {
+        "Double Strike": attack_skill
+    }
     if check:
         print(char["name"], "has leveled up!\n\nNew stats:")
-        char["max_health"] *= 0.5
-        char["health"] *= 0.5
-        char["strength"] *= 0.5
-        char["defense"] *= 0.5
-        char["dexterity"] *= 0.5
+        char["max_health"] *= 1.5
+        char["health"] *= 1.5
+        char["strength"] *= 1.5
+        char["defense"] *= 1.5
+        char["dexterity"] *= 1.5
         char["level"] = char["level"] + 1
         char["exp"] = 0
+        if char['level'] == 2:
+            char["skills"].append(attack_skill.__name__)
         display_character_info(char)
     return char
 
 
-def execute_challenge_protocol(char):
+def execute_challenge_protocol(char):  #
     print("\n***\nCombat has initiated\n***\n")
     enemy = return_entity(char["level"])
     while enemy["health"] > 0 and char["health"] > 0:
@@ -109,11 +124,17 @@ def execute_challenge_protocol(char):
         action = input("Please enter an action:")
         if action == '1':
             attack(char, enemy)
+        if action == '2':
+            eval(char['skills'][0])(char, enemy)
         if enemy["health"] <= 0:
             char["exp"] += enemy["exp"]
             print("\n" + enemy["name"], "had been defeated\n")
             break
         attack(enemy, char)
+        if char["health"] <= 0:
+            char["exp"] -= enemy["exp"]
+            print("\n" + char["name"], "had been defeated\n\nYou have lost some exp")
+            break
     print("\n***\nCombat had ended\n***\n")
     char["health"] = char["max_health"]
     return char
