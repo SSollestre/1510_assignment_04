3692ada25adbf89adb08d0c1fe5390c17775f143
Emildore <Emily.tran.1995@gmail.com>
2022-12-02 14:29:52 -0800

    add copy scenarios

--- map.py
+++ map.py
@@ -71,112 +71,112 @@ scenario = {
             '*Safe Zone*',
     (0, 1): 'You hear scuffling and voices.*Safe Zone*',
     (0, 2): 'It`s very dark, you can`t make out anything.',
-    (0, 3): '',
-    (0, 4): '',
-    (0, 5): '',
-    (0, 6): '',
-    (0, 7): '',
-    (0, 8): '',
-    (0, 9): '',
+    (0, 3): 'The cave is dark and damp',
+    (0, 4): 'The cave is dark and damp',
+    (0, 5): 'The cave is dark and damp',
+    (0, 6): 'The cave is dark and damp',
+    (0, 7): 'The cave is dark and damp',
+    (0, 8): 'The cave is dark and damp',
+    (0, 9): 'The cave is dark and damp',
 
     (1, 0): 'You hear scuffling and voices.*Safe Zone*',
     (1, 1): 'You hear scuffling and voices.*Safe Zone*',
-    (1, 2): '',
-    (1, 3): '',
-    (1, 4): '',
-    (1, 5): '',
-    (1, 6): '',
-    (1, 7): '',
-    (1, 8): '',
-    (1, 9): '',
+    (1, 2): 'The cave is dark and damp',
+    (1, 3): 'The cave is dark and damp',
+    (1, 4): 'The cave is dark and damp',
+    (1, 5): 'The cave is dark and damp',
+    (1, 6): 'The cave is dark and damp',
+    (1, 7): 'The cave is dark and damp',
+    (1, 8): 'The cave is dark and damp',
+    (1, 9): 'The cave is dark and damp',
 
-    (2, 0): '',
-    (2, 1): '',
+    (2, 0): 'The cave is dark and damp',
+    (2, 1): 'The cave is dark and damp',
     (2, 2): 'It`s very dark, you can`t make out anything.',
-    (2, 3): '',
-    (2, 4): '',
-    (2, 5): '',
-    (2, 6): '',
-    (2, 7): '',
-    (2, 8): '',
-    (2, 9): '',
+    (2, 3): 'The cave is dark and damp',
+    (2, 4): 'The cave is dark and damp',
+    (2, 5): 'The cave is dark and damp',
+    (2, 6): 'The cave is dark and damp',
+    (2, 7): 'The cave is dark and damp',
+    (2, 8): 'The cave is dark and damp',
+    (2, 9): 'The cave is dark and damp',
 
-    (3, 0): '',
-    (3, 1): '',
-    (3, 2): '',
-    (3, 3): '',
-    (3, 4): '',
-    (3, 5): '',
-    (3, 6): '',
-    (3, 7): '',
-    (3, 8): '',
-    (3, 9): '',
+    (3, 0): 'The cave is dark and damp',
+    (3, 1): 'The cave is dark and damp',
+    (3, 2): 'The cave is dark and damp',
+    (3, 3): 'The cave is dark and damp',
+    (3, 4): 'The cave is dark and damp',
+    (3, 5): 'The cave is dark and damp',
+    (3, 6): 'The cave is dark and damp',
+    (3, 7): 'The cave is dark and damp',
+    (3, 8): 'The cave is dark and damp',
+    (3, 9): 'The cave is dark and damp',
 
-    (4, 0): '',
-    (4, 1): '',
-    (4, 2): '',
-    (4, 3): '',
-    (4, 4): '',
-    (4, 5): '',
-    (4, 6): '',
-    (4, 7): '',
-    (4, 8): '',
-    (4, 9): '',
+    (4, 0): 'The cave is dark and damp',
+    (4, 1): 'The cave is dark and damp',
+    (4, 2): 'The cave is dark and damp',
+    (4, 3): 'The cave is dark and damp',
+    (4, 4): 'The cave is dark and damp',
+    (4, 5): 'The cave is dark and damp',
+    (4, 6): 'The cave is dark and damp',
+    (4, 7): 'The cave is dark and damp',
+    (4, 8): 'The cave is dark and damp',
+    (4, 9): 'The cave is dark and damp',
 
-    (5, 0): '',
-    (5, 1): '',
-    (5, 2): '',
-    (5, 3): '',
-    (5, 4): '',
-    (5, 5): '',
-    (5, 6): '',
-    (5, 7): '',
-    (5, 8): '',
-    (5, 9): '',
+    (5, 0): 'The cave is dark and damp',
+    (5, 1): 'The cave is dark and damp',
+    (5, 2): 'The cave is dark and damp',
+    (5, 3): 'The cave is dark and damp',
+    (5, 4): 'The cave is dark and damp',
+    (5, 5): 'The cave is dark and damp',
+    (5, 6): 'The cave is dark and damp',
+    (5, 7): 'The cave is dark and damp',
+    (5, 8): 'The cave is dark and damp',
+    (5, 9): 'The cave is dark and damp',
 
-    (6, 0): '',
-    (6, 1): '',
-    (6, 2): '',
-    (6, 3): '',
-    (6, 4): '',
-    (6, 5): '',
-    (6, 6): '',
-    (6, 7): '',
-    (6, 8): '',
-    (6, 9): '',
+    (6, 0): 'The cave is dark and damp',
+    (6, 1): 'The cave is dark and damp',
+    (6, 2): 'The cave is dark and damp',
+    (6, 3): 'The cave is dark and damp',
+    (6, 4): 'The cave is dark and damp',
+    (6, 5): 'The cave is dark and damp',
+    (6, 6): 'The cave is dark and damp',
+    (6, 7): 'The cave is dark and damp',
+    (6, 8): 'The cave is dark and damp',
+    (6, 9): 'The cave is dark and damp',
 
-    (7, 0): '',
-    (7, 1): '',
-    (7, 2): '',
-    (7, 3): '',
-    (7, 4): '',
-    (7, 5): '',
-    (7, 6): '',
-    (7, 7): '',
-    (7, 8): '',
-    (7, 9): '',
+    (7, 0): 'The cave is dark and damp',
+    (7, 1): 'The cave is dark and damp',
+    (7, 2): 'The cave is dark and damp',
+    (7, 3): 'The cave is dark and damp',
+    (7, 4): 'The cave is dark and damp',
+    (7, 5): 'The cave is dark and damp',
+    (7, 6): 'The cave is dark and damp',
+    (7, 7): 'The cave is dark and damp',
+    (7, 8): 'The cave is dark and damp',
+    (7, 9): 'The cave is dark and damp',
 
-    (8, 0): '',
-    (8, 1): '',
-    (8, 2): '',
-    (8, 3): '',
-    (8, 4): '',
-    (8, 5): '',
-    (8, 6): '',
-    (8, 7): '',
-    (8, 8): '',
-    (8, 9): '',
+    (8, 0): 'The cave is dark and damp',
+    (8, 1): 'The cave is dark and damp',
+    (8, 2): 'The cave is dark and damp',
+    (8, 3): 'The cave is dark and damp',
+    (8, 4): 'The cave is dark and damp',
+    (8, 5): 'The cave is dark and damp',
+    (8, 6): 'The cave is dark and damp',
+    (8, 7): 'The cave is dark and damp',
+    (8, 8): 'The cave is dark and damp',
+    (8, 9): 'The cave is dark and damp',
 
-    (9, 0): '',
-    (9, 1): '',
-    (9, 2): '',
-    (9, 3): '',
-    (9, 4): '',
-    (9, 5): '',
-    (9, 6): '',
-    (9, 7): '',
-    (9, 8): '',
-    (9, 9): '',
+    (9, 0): 'The cave is dark and damp',
+    (9, 1): 'The cave is dark and damp',
+    (9, 2): 'The cave is dark and damp',
+    (9, 3): 'The cave is dark and damp',
+    (9, 4): 'The cave is dark and damp',
+    (9, 5): 'The cave is dark and damp',
+    (9, 6): 'The cave is dark and damp',
+    (9, 7): 'The cave is dark and damp',
+    (9, 8): 'The cave is dark and damp',
+    (9, 9): 'The cave is dark and damp',
 
 }
 
@@ -362,6 +362,7 @@ paths = [((0, 0), (0, 1)),
          ((9, 8), (9, 9))]
 m = Map(10, 10, 0, 0, paths)
 
+
 def chance_encounter():
     number = random.randint(0, 10)
     for char["level"] in range(0, 4):
@@ -376,8 +377,8 @@ def chance_encounter():
 def main():
     character = encounter.make_character()
     while True:
-        direction = input("What direction do you want to move? [w/d/s/a] ")
-        m.move(direction)
+        directions = ['w', 'a', 's', 'd']
+        # commands = ['1','2','3','q']
         m.print_scenario()
         encounter.execute_glowup_protocol(character)
         valid_moves = enumerate(["Display character information", "Start encounter", "Display Map"], 1)
@@ -385,7 +386,7 @@ def main():
         for count, value in valid_moves:
             print(str(count) + ":", value)
         print("q: Quit")
-        move = input("\nPlease enter an action:")
+        move = input("\nPlease enter an action or direction [w, a, s, d]:")
         if move == '1':
             encounter.display_character_info(character)
         elif move == '2':
@@ -394,6 +395,8 @@ def main():
             m.print_map()
         elif move == 'q':
             break
+        elif move in directions:
+            m.move(move)
 
 
 if __name__ == "__main__":
