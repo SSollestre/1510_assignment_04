5b6bb422e06174cdc1438a134faad5bfc5a1c22a
Emildore <Emily.tran.1995@gmail.com>
2022-12-03 17:40:25 -0800

    create unittests

--- test_print_scenario.py
+++ test_print_scenario.py
@@ -8,7 +8,33 @@ from game import print_scenario
 class PrintScenarioTest(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_scenario(self, mock_stdout):
+    def test_print_scenario_zero(self, mock_stdout):
         print_scenario(Map(2, 2, 0, 0, [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((1, 0), (1, 1)), ((0, 1), (1, 1))]))
-        expected = 'Current location (0, 0)''you wake up in a dark damp cave.''You remember being knocked out.''It seems that you have been kidnapped.'
+        expected = 'Current location (0, 0)\n' \
+                   'You wake up in a dark damp cave. You remember being knocked out.' \
+                   ' It seems that you have been kidnapped.\n'
+        self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_scenario_column(self, mock_stdout):
+        print_scenario(Map(2, 2, 1, 0, [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((1, 0), (1, 1)), ((0, 1), (1, 1))]))
+        expected = 'Current location (0, 1)\nYou hear scuffling and voices.\n'
+        self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_scenario_row(self, mock_stdout):
+        print_scenario(Map(2, 2, 0, 1, [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((1, 0), (1, 1)), ((0, 1), (1, 1))]))
+        expected = 'Current location (1, 0)\nYou hear scuffling and voices.\n'
+        self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_scenario_both_even(self, mock_stdout):
+        print_scenario(Map(2, 2, 2, 2, [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((1, 0), (1, 1)), ((0, 1), (1, 1))]))
+        expected = 'Current location (2, 2)\nIt`s very dark, you can`t make out anything.\n'
+        self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_scenario_both_odd(self, mock_stdout):
+        print_scenario(Map(2, 2, 1, 1, [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((1, 0), (1, 1)), ((0, 1), (1, 1))]))
+        expected = 'Current location (1, 1)\nYou hear scuffling and voices.\n'
         self.assertEqual(mock_stdout.getvalue(), expected)
