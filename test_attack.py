596be3bbec37b7af83a7dd2d9d42a9764c430817
SSollestre <sean.sollestre@gmail.com>
2022-12-03 17:49:43 -0800

    Implement self attributes

--- test_attack.py
+++ test_attack.py
@@ -40,13 +40,7 @@ class TestAttack(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_attack_player_to_enemy(self, mock_output):
-        character = self.character
-        enemy = self.enemy
-        actual = attack(character, enemy)
-        system_print = mock_output.getvalue()
-        expected_print = "Player strikes Bandit for 7.5 damage!\n"
-
-        expected = {
+        expected_return = {
             "name": "Bandit",
             "health": 42.5,
             "strength": 10,
@@ -56,43 +50,15 @@ class TestAttack(TestCase):
             "goal": False,
             "exp": 25
         }
-
+        expected_print = f"{self.character['name']} strikes {self.enemy['name']} for 7.5 damage!\n"
+        actual_return = attack(self.character, self.enemy)
+        system_print = mock_output.getvalue()
         self.assertEqual(expected_print, system_print)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected_return, actual_return)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_attack_enemy_to_player(self, mock_output):
-        character = {
-            "name": "Player",
-            "max_health": 50,
-            "health": 50,
-            "strength": 10,
-            "defense": 10,
-            "dexterity": 10,
-            "level": 1,
-            "exp": 0,
-            "goal": False,
-            "skills": {
-                "Basic Attack": attack
-            }
-        }
-
-        enemy = {
-            "name": "Bandit",
-            "health": 50,
-            "strength": 10,
-            "defense": 10,
-            "dexterity": 10,
-            "level": 1,
-            "goal": False,
-            "exp": 25
-        }
-
-        actual = attack(enemy, character)
-        system_print = mock_output.getvalue()
-        expected_print = "Bandit strikes Player for 7.5 damage!\n"
-
-        expected = {
+        expected_return = {
             "name": "Player",
             "max_health": 50,
             "health": 42.5,
@@ -106,9 +72,11 @@ class TestAttack(TestCase):
                 "Basic Attack": attack
             }
         }
-
+        expected_print = f"{self.enemy['name']} strikes {self.character['name']} for 7.5 damage!\n"
+        actual_return = attack(self.enemy, self.character)
+        system_print = mock_output.getvalue()
         self.assertEqual(expected_print, system_print)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected_return, actual_return)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_attack_no_damage(self, mock_output):
