f4f2314e3445877db70283fa4666e0e8199027be
SSollestre <sean.sollestre@gmail.com>
2022-12-03 18:06:07 -0800

    Implement setup attributes

--- test_attack.py
+++ test_attack.py
@@ -34,23 +34,23 @@ class TestAttack(TestCase):
             "exp": 25
         }
         self.lieutenant = {
-            "name": "Bandit",
+            "name": "Bandit Lieutenant",
             "health": 50,
-            "strength": 10,
+            "strength": 30,
             "defense": 10,
             "dexterity": 10,
-            "level": 1,
+            "level": 2,
             "goal": False,
             "exp": 25
         }
         self.chief = {
-            "name": "Bandit",
-            "health": 50,
-            "strength": 10,
-            "defense": 10,
+            "name": "Bandit Chief",
+            "health": 150,
+            "strength": 30,
+            "defense": 20,
             "dexterity": 10,
-            "level": 1,
-            "goal": False,
+            "level": 3,
+            "goal": True,
             "exp": 25
         }
 
@@ -66,7 +66,8 @@ class TestAttack(TestCase):
             "goal": False,
             "exp": 25
         }
-        expected_print = f"{self.character['name']} strikes {self.bandit['name']} for 7.5 damage!\n"
+        damage = self.character["strength"] - (0.25 * self.bandit["defense"])
+        expected_print = f"{self.character['name']} strikes {self.bandit['name']} for {damage} damage!\n"
         actual_return = attack(self.character, self.bandit)
         system_print = mock_output.getvalue()
         self.assertEqual(expected_print, system_print)
@@ -88,8 +89,55 @@ class TestAttack(TestCase):
                 "Basic Attack": attack
             }
         }
-        expected_print = f"{self.bandit['name']} strikes {self.character['name']} for 7.5 damage!\n"
+        damage = self.bandit["strength"] - (0.25 * self.character["defense"])
+        expected_print = f"{self.bandit['name']} strikes {self.character['name']} for {damage} damage!\n"
         actual_return = attack(self.bandit, self.character)
+        system_print = mock_output.getvalue()
+        self.assertEqual(expected_print, system_print)
+        self.assertEqual(expected_return, actual_return)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_attack_lieutenant_to_player(self, mock_output):
+        expected_return = {
+            "name": "Player",
+            "max_health": 50,
+            "health": 22.5,
+            "strength": 10,
+            "defense": 10,
+            "dexterity": 10,
+            "level": 1,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack
+            }
+        }
+        damage = self.lieutenant["strength"] - (0.25 * self.character["defense"])
+        expected_print = f"{self.lieutenant['name']} strikes {self.character['name']} for {damage} damage!\n"
+        actual_return = attack(self.lieutenant, self.character)
+        system_print = mock_output.getvalue()
+        self.assertEqual(expected_print, system_print)
+        self.assertEqual(expected_return, actual_return)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_attack_chief_to_player(self, mock_output):
+        expected_return = {
+            "name": "Player",
+            "max_health": 50,
+            "health": 22.5,
+            "strength": 10,
+            "defense": 10,
+            "dexterity": 10,
+            "level": 1,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack
+            }
+        }
+        damage = self.chief["strength"] - (0.25 * self.character["defense"])
+        expected_print = f"{self.chief['name']} strikes {self.character['name']} for {damage} damage!\n"
+        actual_return = attack(self.chief, self.character)
         system_print = mock_output.getvalue()
         self.assertEqual(expected_print, system_print)
         self.assertEqual(expected_return, actual_return)
