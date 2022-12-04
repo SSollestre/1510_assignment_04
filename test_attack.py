ec256a82bd69bd7f578e3250383b8137531aea3b
SSollestre <sean.sollestre@gmail.com>
2022-12-03 17:50:52 -0800

    Remove unnecessary test

--- test_attack.py
+++ test_attack.py
@@ -23,7 +23,6 @@ class TestAttack(TestCase):
                 "Basic Attack": attack
             }
         }
-
         enemy = {
             "name": "Bandit",
             "health": 50,
@@ -34,7 +33,6 @@ class TestAttack(TestCase):
             "goal": False,
             "exp": 25
         }
-
         self.character = character
         self.enemy = enemy
 
@@ -77,46 +75,3 @@ class TestAttack(TestCase):
         system_print = mock_output.getvalue()
         self.assertEqual(expected_print, system_print)
         self.assertEqual(expected_return, actual_return)
-
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_attack_no_damage(self, mock_output):
-        character = {
-            "name": "Player",
-            "max_health": 50,
-            "health": 50,
-            "strength": 0,
-            "defense": 10,
-            "dexterity": 10,
-            "level": 1,
-            "exp": 0,
-            "goal": False,
-            "skills": {
-                "Basic Attack": attack
-            }
-        }
-
-        enemy = {
-            "name": "Bandit",
-            "health": 50,
-            "strength": 10,
-            "defense": 0,
-            "dexterity": 10,
-            "level": 1,
-            "goal": False,
-            "exp": 25
-        }
-
-        actual = attack(character, enemy)
-
-        expected = {
-            "name": "Bandit",
-            "health": 50,
-            "strength": 10,
-            "defense": 0,
-            "dexterity": 10,
-            "level": 1,
-            "goal": False,
-            "exp": 25
-        }
-
-        self.assertEqual(expected, actual)
