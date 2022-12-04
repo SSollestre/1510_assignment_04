8c767a9ac7d8b2560b36f1cdb4888de6420bfa71
SSollestre <sean.sollestre@gmail.com>
2022-12-03 19:27:50 -0800

    Add unit tests to scale values

--- test_scale_values.py
+++ test_scale_values.py
@@ -1,8 +1,79 @@
 from unittest import TestCase
+from unittest.mock import patch
+import io
 
-from encounter import scale_values
+
+from encounter import scale_values, attack, double_strike, guard
 
 
-class ScaleValuesTest(TestCase):
-    def test_scale_values(self):
-        self.fail()
+class TestScaleValues(TestCase):
+    def setUp(self) -> None:
+        self.character_one = {
+            "name": "Player",
+            "max_health": 50,
+            "health": 50,
+            "strength": 10,
+            "defense": 10,
+            "dexterity": 10,
+            "level": 1,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack
+            }
+        }
+
+        self.character_two = {
+            "name": "Player",
+            "max_health": int(50 * 1.5),
+            "health": int(50 * 1.5),
+            "strength": int(10 * 1.5),
+            "defense": int(10 * 1.5),
+            "dexterity": int(10 * 1.5),
+            "level": 2,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack,
+                "Double Strike": double_strike
+            }
+        }
+
+    def test_scale_values_level_two(self):
+        expected = {
+            "name": "Player",
+            "max_health": 50 * 1.5,
+            "health": 50 * 1.5,
+            "strength": 10 * 1.5,
+            "defense": 10 * 1.5,
+            "dexterity": 10 * 1.5,
+            "level": 2,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack,
+                "Double Strike": double_strike
+            }
+        }
+        actual = scale_values(self.character_one)
+        self.assertEqual(expected, actual)
+
+    def test_scale_values_level_three(self):
+        expected = {
+            "name": "Player",
+            "max_health": int((50 * 1.5) * 1.5),
+            "health": int(((50 * 1.5) * 1.5)),
+            "strength": int((10 * 1.5) * 1.5),
+            "defense": int((10 * 1.5) * 1.5),
+            "dexterity": int((10 * 1.5) * 1.5),
+            "level": 3,
+            "exp": 0,
+            "goal": False,
+            "skills": {
+                "Basic Attack": attack,
+                "Double Strike": double_strike,
+                "Guard": guard
+            }
+        }
+        actual = scale_values(self.character_two)
+        self.assertEqual(expected, actual)
